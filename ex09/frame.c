412165201853afd7725c8132fbbbef1797b10819
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2023-04-02 00:46:23 +0000

    fix 9

--- ex09/frame.c
+++ ex09/frame.c
@@ -3,41 +3,48 @@
 
 int main(int argc, char **argv)
 {
-    if(argc == 2)
+    if(argc >= 2)
     {
-        char *string = strtok(argv[1], " ");
-        int flag = 0;
-        size_t len = strlen(string) + 4;
-        while(string != NULL)
+        for(int i = 1; i < argc ; i++)
         {
-            int j = 0;
-            if(flag == 0)
+            if(strcmp(argv[i], ""))
             {
+
+                char *string = strtok(argv[i], " ");
+                int flag = 0;
+                size_t len = strlen(string) + 4;
+                while(string != NULL)
+                {
+                    int j = 0;
+                    if(flag == 0)
+                    {
+                        while(j < len)
+                        {
+                            printf("*");
+                            j++;
+                        }
+                        printf("\n");
+                    }
+                    flag = 1;
+                    printf("* ");
+                    printf("%s" , string);
+                    int k = 0;
+                    while(k < len - strlen(string) - 4)
+                    {
+                        printf(" ");
+                        k++;
+                    }
+                    printf(" *\n");
+                    string = strtok(NULL, " ");
+                }
+                int j = 0;
                 while(j < len)
                 {
                     printf("*");
                     j++;
                 }
                 printf("\n");
-            }
-            flag = 1;
-            printf("* ");
-            printf("%s" , string);
-            int k = 0;
-            while(k < len - strlen(string) - 4)
-            {
-                printf(" ");
-                k++;
-            }
-            printf(" *\n");
-            string = strtok(NULL, " ");
         }
-        int j = 0;
-        while(j < len)
-        {
-            printf("*");
-            j++;
         }
-        printf("\n");
     }
 }
