0c95b176a24d7fed3b48f08e7504438993c9d31d
Oumaima Fisaoui <oufisaou@e2r2p14.1337.ma>
2023-04-02 23:44:21 +0000

    e

--- ex08/blackjack.c
+++ ex08/blackjack.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char **argv)
 {
@@ -9,27 +10,31 @@ int main(int argc, char **argv)
     {
         for(int i = 0; i < strlen(argv[1]); i++)
         {
-            if(isdigit(argv[1][i]) && argv[1][i] >= 2 && argv[1][i] <= 9)
+            if(isdigit(argv[1][i]) && (argv[1][i] - '0') >= 2 && (argv[1][i] - '0') <= 9)
             {
-                result += argv[1][i];
+                result += argv[1][i] - '0';
             }
-            else if (!strcmp(argv[1][i], 'J') || !strcmp(argv[1][i], 'Q') ||!strcmp(argv[1][i], 'K'))
+            else if (argv[1][i] == 'J' || argv[1][i] ==  'Q' || argv[1][i] ==  'K' || argv[1][i] ==  'D' || argv[1][i] == 'T')
             {
                 result += 10;
             }
-            else if (!strcmp(argv[1][i], 'A'))
+            else if (argv[1][i] ==  'A')
             {
+                result += 11;
                 ace++;
             }
         }
-        if(ace > 1)
+        while(ace > 0 && result > 21)
         {
-            
+            result -= 10;
+            ace--;
         }
-        else if (ace == 0)
+        if (result == 21)
         {
-            return result;
+            printf("Blackjack!\n");
         }
+        else
+            printf("%d\n", result);
     }
-    return (1);
+
 }
