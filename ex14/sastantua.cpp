9b4781bdb328035c3e7ebed417efbbafcb7c1cd1
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2023-04-03 23:23:32 +0000

    sas

--- ex14/sastantua.cpp
+++ ex14/sastantua.cpp
@@ -1,69 +1,88 @@
-#include <iostream>
-#include <cmath>
 
-//there is a math behind it
+#include <iostream>
+#include <string>
 using namespace std;
 
-void printRow(int spaces, int blocks) {
-    // prints a row of the pyramid
-    for (int i = 0; i < spaces; i++) {
-        cout << " ";
+bool is_digits(const std::string& str)
+{
+    for (std::string::size_type i = 0; i < str.length(); i++) {
+        if (!isdigit(str[i])) {
+            return false;
+        }
     }
-    cout << "/";
-    for (int i = 0; i < blocks; i++) {
-        cout << "*";
-    }
-    cout << "\\" << endl;
+    return true;
 }
 
-void drawSastantua(int height) {
-    int row = 1; // current row number
-    int base = 3; // number of blocks in the first row of the pyramid
-    int blocks = base; // number of blocks in the current row
-    int spaces; // number of spaces before the first block in the current row
+int main(int argc, char **argv) 
+{
+    if(argc == 2)
+    {
+        std::string arg = argv[1];
+        if(is_digits(arg))
+        {
+            int height = stoi(arg);
 
-    // loop through each row of the pyramid
-    while (row <= height) {
-        // calculate number of spaces before the first block in the current row
-        spaces = (pow(2, height + 2) - pow(2, row + 1)) / 2;
 
-        // print top section of the row
-        printRow(spaces, blocks);
 
-        // calculate number of blocks in the middle section of the row
-        if (row >= base) {
-            blocks += (row % 2 == 0) ? row + 1 : row;
-        }
+            if(height > 1 && height <=5)
+            {
+                int rows = height + 2;
+                int init = 4;
 
-        // print middle section of the row
-        if (row < height) {
-            for (int i = 0; i < pow(2, row - 1); i++) {
-                spaces--;
-                blocks--;
-                printRow(spaces, blocks);
+                int flag = 0;
+                for (int i = 1; i <= rows; i++)
+                {
+                    if(i == rows)
+                        flag = 1;
+                    //spaces
+                    for(int j = 1; j <= rows - i; j++)
+                    {
+                        cout << " ";
+
+                    }
+                    cout << "/";
+                    for(int k = 1; k <= (i * 2) - 1; k++)
+                    {
+                        if(flag == 1 && k == i)
+                        {
+                            cout << "|";
+                            continue;
+                        }
+                        else
+                            cout << "*";
+                    }
+                    cout << "\\";
+                    cout << endl;
+                }
+
             }
-        }
+            // int height = stoi(arg);
+            // int rows = height + 2;
+            // int init = 4;
 
-        // calculate number of blocks in the bottom section of the row
-        if (row >= base) {
-            blocks += (row % 2 == 0) ? row + 1 : row;
-        }
+            // int flag = 0;
+            // for (int i = 1; i <= rows; i++)
+            // {
+            //     if(i == rows)
+            //         flag = 1;
+            //     for(int j = 1; j <= init - i - 1; j++)
+            //     {
+            //         cout << " ";
 
-        // print bottom section of the row
-        if (row < height) {
-            printRow(spaces, blocks);
+            //     }
+            //     cout << "/";
+            //     for(int k = 1; k <= i; k++)
+            //     {
+            //         if(flag == 1 && k == init - 1)
+            //         {
+            //             cout << "|";
+            //             break ;
+            //         }
+            //         else
+            //             cout << "*";
+            //     }
+            //     cout << endl;
+            // }
         }
-
-        row++; // move to the next row
-    }
 }
-
-int main() {
-    int height;
-    cout << "Enter pyramid height: ";
-    cin >> height;
-
-    drawSastantua(height);
-
-    return 0;
 }
